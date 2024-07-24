474277dda08351e08da61000398ed5c88ac926d0
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-07-24 21:57:57 +0200

    Update BakeyourbreadFoodComponents.java

--- src/main/java/de/pnku/bakeyourbread/item/BakeyourbreadFoodComponents.java
+++ src/main/java/de/pnku/bakeyourbread/item/BakeyourbreadFoodComponents.java
@@ -3,18 +3,17 @@ package de.pnku.bakeyourbread.item;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.Items;
 
 public class BakeyourbreadFoodComponents {
-    public static final FoodProperties COOKIE_DOUGH = (new FoodProperties.Builder()).nutrition(1).saturationModifier(0.1F).effect(new MobEffectInstance(MobEffects.HUNGER, 100, 0), 0.1F).effect(new MobEffectInstance(MobEffects.CONFUSION, 100, 0), 0.1F).build();
-    public static final FoodProperties BREAD_DOUGH = (new FoodProperties.Builder()).nutrition(2).saturationModifier(0.5F).effect(new MobEffectInstance(MobEffects.HUNGER, 100, 0), 0.1F).effect(new MobEffectInstance(MobEffects.CONFUSION, 100, 0), 0.1F).build();
-    public static final FoodProperties PUMPKIN_PIE_DOUGH = (new FoodProperties.Builder()).nutrition(4).saturationModifier(0.2F).effect(new MobEffectInstance(MobEffects.POISON, 300, 1), 0.15F).effect(new MobEffectInstance(MobEffects.HUNGER, 200, 1), 0.2F).effect(new MobEffectInstance(MobEffects.CONFUSION, 100, 0), 0.2F).build();
-    public static final FoodProperties UNBAKED_CAKE = (new FoodProperties.Builder()).nutrition(6).saturationModifier(0.6F).effect(new MobEffectInstance(MobEffects.POISON, 300, 0), 0.15F).effect(new MobEffectInstance(MobEffects.HUNGER, 200, 1), 0.1F).effect(new MobEffectInstance(MobEffects.CONFUSION, 100, 0), 0.25F).build();
+    public static final FoodProperties COOKIE_DOUGH = (new FoodProperties.Builder()).nutrition(1).saturationMod(0.1F).effect(new MobEffectInstance(MobEffects.HUNGER, 100, 0), 0.1F).effect(new MobEffectInstance(MobEffects.CONFUSION, 100, 0), 0.1F).build();
+    public static final FoodProperties BREAD_DOUGH = (new FoodProperties.Builder()).nutrition(2).saturationMod(0.5F).effect(new MobEffectInstance(MobEffects.HUNGER, 100, 0), 0.1F).effect(new MobEffectInstance(MobEffects.CONFUSION, 100, 0), 0.1F).build();
+    public static final FoodProperties PUMPKIN_PIE_DOUGH = (new FoodProperties.Builder()).nutrition(4).saturationMod(0.2F).effect(new MobEffectInstance(MobEffects.POISON, 300, 1), 0.15F).effect(new MobEffectInstance(MobEffects.HUNGER, 200, 1), 0.2F).effect(new MobEffectInstance(MobEffects.CONFUSION, 100, 0), 0.2F).build();
+    public static final FoodProperties UNBAKED_CAKE = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.6F).effect(new MobEffectInstance(MobEffects.POISON, 300, 0), 0.15F).effect(new MobEffectInstance(MobEffects.HUNGER, 200, 1), 0.1F).effect(new MobEffectInstance(MobEffects.CONFUSION, 100, 0), 0.25F).build();
     public static final FoodProperties UNCOOKED_BEETROUP_SOUP = uncookedStew(5).build();
     public static final FoodProperties UNCOOKED_MUSHROOM_STEW = uncookedStew(4).build();
     public static final FoodProperties UNCOOKED_RABBIT_STEW = uncookedStew(5).effect(new MobEffectInstance(MobEffects.POISON, 300, 1), 0.05F).effect(new MobEffectInstance(MobEffects.HUNGER, 300, 0), 0.15F).build();
 
     private static FoodProperties.Builder uncookedStew(int nutrition) {
-        return (new FoodProperties.Builder()).nutrition(nutrition).saturationModifier(0.4F).usingConvertsTo(Items.BOWL);
+        return (new FoodProperties.Builder()).nutrition(nutrition).saturationMod(0.4F);
     }
 }
