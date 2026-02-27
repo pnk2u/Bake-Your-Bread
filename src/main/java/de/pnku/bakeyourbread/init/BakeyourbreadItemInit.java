4056916e98d33e3fd4ea02726c560b0600ec5f4f
pnku <159317511+pnk2u@users.noreply.github.com>
2026-02-27 10:15:59 +0100

    Minor changes to mod/item init (1.3.0-d:c7)

--- src/main/java/de/pnku/bakeyourbread/init/BakeyourbreadItemInit.java
+++ src/main/java/de/pnku/bakeyourbread/init/BakeyourbreadItemInit.java
@@ -21,12 +21,15 @@ public class BakeyourbreadItemInit {
 
 
     public static void registerUncookedFoods() {
+        LOGGER.info("Stews, Soups and Carrots are being cooked...");
         registerUncookedFood(Items.BEETROOT_SOUP, UNCOOKED_BEETROOT_SOUP);
         registerUncookedFood(Items.MUSHROOM_STEW, UNCOOKED_MUSHROOM_STEW);
         registerUncookedFood(Items.RABBIT_STEW, UNCOOKED_RABBIT_STEW);
-
+    }
+    public static void registerCookedFoods() {
     }
     public static void registerUnbakedFoods() {
+        LOGGER.info("Bread, Cakes, Cookies and Pies are being baked...");
         registerUnbakedFood(Items.BREAD, UNBAKED_BREAD);
         registerUnbakedFood(Items.CAKE, UNBAKED_CAKE);
         registerUnbakedFood(Items.COOKIE, UNBAKED_COOKIE);
@@ -38,14 +41,18 @@ public class BakeyourbreadItemInit {
         Registry.register(BuiltInRegistries.ITEM, BakeYourBread.withModId("uncooked_" + heatedItem.getDescriptionId().replaceFirst("item.minecraft.", "")), rawItem);
 
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(entries -> entries.addBefore(heatedItem, rawItem));
-        LOGGER.info("Stews and soups are cooking...");
+    }
+
+    private static void registerCookedFood(Item rawItem, Item heatedItem) {
+        Registry.register(BuiltInRegistries.ITEM, BakeYourBread.withModId("cooked_" + rawItem.getDescriptionId().replaceFirst("item.minecraft.", "")), heatedItem);
+
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(entries -> entries.addAfter(rawItem, heatedItem));
     }
 
     private static void registerUnbakedFood(Item heatedItem, Item rawItem) {
         Registry.register(BuiltInRegistries.ITEM, BakeYourBread.withModId("unbaked_" + heatedItem.getDescriptionId().replaceFirst("item.minecraft.", "").replaceFirst("block.minecraft.","")), rawItem);
 
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(entries -> entries.addBefore(heatedItem, rawItem));
-        LOGGER.info("Breads, cakes, cookies and pies are baking...");
     }
 
 
